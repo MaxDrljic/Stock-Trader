f0690e2c2bdc98929222bbfee0284b5f8eeb8838
MaxDrljic <max_spree@hotmail.com>
2018-12-25 17:30:50 +0100

    Stocks component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="container">
     <app-header></app-header>
-    <div class="row">
-      <div class="col-xs-12">
-        <router-view></router-view>
-      </div>
+    <div class="container">
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -20,15 +18,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 body {
   padding: 30px;
 }
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>The Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">
+          NAME
+          <small>(Price: PRICE)</small>
+        </h5>
+        <input type="number" class="form-control" placeholder="Quantity">
+        <button class="btn btn-success">Buy</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
