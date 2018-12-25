ae648567cb670536e5af72eee7bcd9ab1a47c2e9
MaxDrljic <max_spree@hotmail.com>
2018-12-25 11:54:20 +0100

    Header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="container">
-    <router-view></router-view>
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
@@ -16,5 +27,9 @@ export default {};
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  padding: 30px;
 }
 </style>
