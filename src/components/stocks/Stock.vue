83ed3ec0cd5cd86f105daed46b16f313e60ee2ec
MaxDrljic <max_spree@hotmail.com>
2018-12-26 22:53:31 +0100

    Buy button validation and functionality

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,18 +3,40 @@
     <div class="card" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">
-          NAME
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h5>
-        <input type="number" class="form-control" placeholder="Quantity">
-        <button class="btn btn-success">Buy</button>
+        <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+        <button
+          class="btn btn-success"
+          @click="buyStock"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+        >Buy</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
 </script>
 
 <style>
