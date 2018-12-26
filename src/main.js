3b1f31a85f024ffcde71449f352437b00bb82935
MaxDrljic <max_spree@hotmail.com>
2018-12-26 23:24:13 +0100

    Implemented Vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import { routes } from './routes'
 import App from './App.vue'
+import { routes } from './routes'
+import store from './store/store'
 
 Vue.config.productionTip = false
 
@@ -15,5 +16,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
