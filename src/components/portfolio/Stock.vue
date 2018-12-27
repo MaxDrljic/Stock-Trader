0062e86867ce4ea3c74a8649a53d1e4af9aaf263
MaxDrljic <max_spree@hotmail.com>
2018-12-27 11:45:19 +0100

    Connected portfolio to vuex

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,9 +1,47 @@
 <template>
-  <h1>The Portfolio - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+        </h5>
+        <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+        <button
+          class="btn btn-success"
+          @click="sellStock"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+        >Sell</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    ...mapActions({
+      placeSellOrder: "sellStock"
+    }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.placeSellOrder(order);
+      this.quantity = 0;
+    }
+  }
+};
 </script>
 
 <style>
