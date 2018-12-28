70861862e9af1bb1c67c8664e186c77ca33ab659
MaxDrljic <max_spree@hotmail.com>
2018-12-28 12:41:08 +0100

    End day functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,7 @@
       <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
       <ul class="nav navbar-nav navbar-right">
         <li>
-          <a href="#" class="nav-link">End Day</a>
+          <a href="#" @click="endDay" class="nav-link">End Day</a>
         </li>
         <li class="dropdown">
           <a
@@ -43,10 +43,18 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   computed: {
     funds() {
       return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions(["randomizeStocks"]),
+    endDay() {
+      this.randomizeStocks();
     }
   }
 };
